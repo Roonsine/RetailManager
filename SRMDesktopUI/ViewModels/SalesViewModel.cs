5e41e6b96b785f3664a8da9db153196842833ebe
Tyler Carron <synyster.rias@gmail.com>
2020-10-29 20:16:38 -0400

    Wire the WPF and Shopping cart model.
    Setup the ability to store products into the cart.

--- SRMDesktopUI/ViewModels/SalesViewModel.cs
+++ SRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -43,8 +43,21 @@ namespace SRMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<string> _cart;
-        public BindingList<string> Cart
+        private ProductModel _selectedProduct;
+
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+            }
+        }
+
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -55,7 +68,7 @@ namespace SRMDesktopUI.ViewModels
         }
 
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -64,13 +77,21 @@ namespace SRMDesktopUI.ViewModels
             { 
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
         public string SubTotal
         {
-            // TODO - Replace with calculation
-            get { return "$0.00"; }
+            get 
+            {
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+                return subTotal.ToString("C"); 
+            }
         }
         public string Tax
         {
@@ -90,15 +111,41 @@ namespace SRMDesktopUI.ViewModels
             {
                 bool output = false;
 
-                // make sure something is selected    
+                // make sure something is selected
+                // make sure there is an item quantity
+                if(ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 return output;
             }
         }
 
         public void AddToCart()
         {
-            
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if(existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            } else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
+
         public bool CanRemoveFromCart
         {
             get
@@ -112,7 +159,9 @@ namespace SRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            SelectedProduct.QuantityInStock += ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
