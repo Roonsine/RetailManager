38399c3497c5fc25eba452eb9ad600a4b49c02b2
Tyler Carron <synyster.rias@gmail.com>
2020-10-30 22:41:30 -0400

    Update EF Database Name
    Updates the EF database name to be EFData.
    Also added a swagger link to the main page

--- SRMDataManager/Models/IdentityModels.cs
+++ SRMDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SRMDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("EFData", throwIfV1Schema: false)
         {
         }
         
