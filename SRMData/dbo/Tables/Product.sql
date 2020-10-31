ef2649d91f2758d92a970c7a7b2e9874234ab434
Tyler Carron <synyster.rias@gmail.com>
2020-10-30 22:21:02 -0400

    Add Foreign Key Relationships
    Adds FK relationships on sql tables

--- SRMData/dbo/Tables/Product.sql
+++ SRMData/dbo/Tables/Product.sql
@@ -7,5 +7,5 @@
 	[QuantityInStock] INT NOT NULL DEFAULT 1,
 	[CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
 	[LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(), 
-    [IsTaxable] BIT NOT NULL DEFAULT 1
+    [IsTaxable] BIT NOT NULL DEFAULT 1 
 )
